48fcd4e8b4152463d8f6fe54272b47d82085d684
AdiAharonov <adi.ahronov@gmail.com>
2020-07-03 19:57:58 +0300

    Updates

---
+++ precache-manifest.ee7d85ea05dc5d3c726c1486eeda28fe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c21069dd49aba1e8467a2018153137",
+    "url": "/ReactProtfolio/index.html"
+  },
+  {
+    "revision": "39f2af010fe8c82c2900",
+    "url": "/ReactProtfolio/static/css/main.1858c9a5.chunk.css"
+  },
+  {
+    "revision": "7bec60376298b5778b66",
+    "url": "/ReactProtfolio/static/js/2.6452ee40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactProtfolio/static/js/2.6452ee40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f2af010fe8c82c2900",
+    "url": "/ReactProtfolio/static/js/main.7eebe84c.chunk.js"
+  },
+  {
+    "revision": "19485643be027e289158",
+    "url": "/ReactProtfolio/static/js/runtime-main.e6dfeba3.js"
+  },
+  {
+    "revision": "3db31919b255ed7261e7329f0f8303a4",
+    "url": "/ReactProtfolio/static/media/NotoSansJP-Bold.3db31919.otf"
+  },
+  {
+    "revision": "b5f8ef0cb10cf4fa6dfc584d29908421",
+    "url": "/ReactProtfolio/static/media/NotoSansJP-Regular.b5f8ef0c.otf"
+  },
+  {
+    "revision": "e5ca8c0ac474df46fe45840707a0c483",
+    "url": "/ReactProtfolio/static/media/RobotoMono-Regular.e5ca8c0a.ttf"
+  },
+  {
+    "revision": "609417a97a8ca809f1726790044974e4",
+    "url": "/ReactProtfolio/static/media/formalImage.609417a9.jpg"
+  },
+  {
+    "revision": "41dd592a18a80b061e3d1492364a684a",
+    "url": "/ReactProtfolio/static/media/logo.41dd592a.svg"
+  },
+  {
+    "revision": "5b66ca7ca1424291ab130169ff82f0cf",
+    "url": "/ReactProtfolio/static/media/venyou.5b66ca7c.PNG"
+  }
+]);
